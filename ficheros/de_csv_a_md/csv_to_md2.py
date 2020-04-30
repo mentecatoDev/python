babe702929b197b4fb0dfd2dea19890cba2fb93f
mentecatoDev <favila@iesromerovargas.com>
2020-04-30 21:58:28 +0200

    Resuelve listados de cursos FCT con excepciones

--- ficheros/de_csv_a_md/csv_to_md2.py
+++ ficheros/de_csv_a_md/csv_to_md2.py
@@ -11,43 +11,59 @@ file = open('hoja01.md', 'w')
 for line in buffer[1:]:
 
     # Título
-    content = '# ' + line[3] + '\n'
-    file.writelines(content)
+    try:
+        content = '# ' + line[3] + '\n'
+        file.writelines(content)
+    except IndexError:
+        continue
 
     # Propuesto por
-    content = '## ' + buffer[0][1] + '\n'
-    file.writelines(content)
-    content = (line[1] + '\n').replace('<coma>', ',')
-    file.writelines(content)
-
+    try:
+        content = '## ' + buffer[0][1] + '\n'
+        file.writelines(content)
+        content = (line[1] + '\n').replace('<coma>', ',')
+        file.writelines(content)
+    except IndexError:
+        pass
     # Temática
-    content = '## ' + buffer[0][2] + '\n'
-    file.writelines(content)
-    content = (line[2] + '\n').replace('<coma>', ',')
-    file.writelines(content)
-
+    try:
+        content = '## ' + buffer[0][2] + '\n'
+        file.writelines(content)
+        content = (line[2] + '\n').replace('<coma>', ',')
+        file.writelines(content)
+    except IndexError:
+        pass
     # Horas
-    content = '## ' + buffer[0][4] + '\n'
-    file.writelines(content)
-    content = (line[4] + '\n').replace('<coma>', ',')
-    file.writelines(content)
-
+    try:
+        content = '## ' + buffer[0][4] + '\n'
+        file.writelines(content)
+        content = (line[4] + '\n').replace('<coma>', ',')
+        file.writelines(content)
+    except IndexError:
+        pass
     # Enlace
-    content = '## ' + buffer[0][5] + '\n'
-    file.writelines(content)  
-    content = (line[5] + '\n').replace('<coma>', ',')
-    file.writelines(content)
-
+    try:
+        content = '## ' + buffer[0][5] + '\n'
+        file.writelines(content)  
+        content = (line[5] + '\n').replace('<coma>', ',')
+        file.writelines(content)
+    except IndexError:
+        pass
     # Observaciones
-    content = '## ' + buffer[0][6] + '\n'
-    file.writelines(content)
-    content = (line[6] + '\n').replace('<coma>', ',')
-    file.writelines(content)
-
+    try:
+        content = '## ' + buffer[0][6] + '\n'
+        file.writelines(content)
+        content = (line[6] + '\n').replace('<coma>', ',')
+        file.writelines(content)
+    except IndexError:
+        pass
     # Alumnos inscritos
-    content = '## ' + buffer[0][7] + '\n'
-    file.writelines(content)
-    content = (line[7] + '\n').replace('<coma>', ',')
-    file.writelines(content)
+    try:
+        content = '## ' + buffer[0][7] + '\n'
+        file.writelines(content)
+        content = (line[7] + '\n').replace('<coma>', ',')
+        file.writelines(content)
+    except IndexError:
+        pass
 
 
